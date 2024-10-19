2d51103d44a87fc1ef11b88579c9f957c81075ef
SaadNabeel <saadnabeel2010@gmail.com>
2024-10-19 14:56:14 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+a = {2, 5, 7, 12, 3, 23, 65, 42, 2, 3}
+b = {12, 3, 5, 7, 2, 55, 34, 65}
+
+print('Union:', a.union(b))  
+print('Intersection:', a.intersection(b))  
+print('Difference:', a.difference(b))  
+print('Symmetric difference:', a.symmetric_difference(b))  
